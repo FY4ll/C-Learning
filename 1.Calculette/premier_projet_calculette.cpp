40a5c7f973b63ba8acb98e8ce79a87ef863e4a92
F4ll_ <fadyallemann@gmail.com>
2026-08-05 00:34:41 +0200

    Switch and Do while swap

--- 1.Calculette/premier_projet_calculette.cpp
+++ 1.Calculette/premier_projet_calculette.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 int main() {
@@ -8,8 +7,11 @@ int main() {
 
     double first_Number, second_Number, resultat;
     char operation;
+    char recommencer;
 
-    // obtention du première nombre 
+    do
+    {
+     // obtention du première nombre 
 
     cout << "Entrée le premier chiffre: ";  // impression du texte pour l'utilisateur
     cin >> first_Number; // permet à l'utilisateur de rentrée des valeur puis on les stock dans first_number
@@ -21,33 +23,39 @@ int main() {
     
     // Operation choisit par l'utilisateur
 
-    cout << "Operation voulu: Multiplication(x), Division(/), Adition(+) ou Soustraction(-)";
-    cin >> operation; 
+    cout << "Operation voulu: Multiplication(x), Division(/), Adition(+) ou Soustraction(-)? ";
+    cin >> operation;
     
-    //mise en place de la condition pour crée notre calculette
-    if (operation = 'x'){
+    //switch afin d'avoir une option pour recommencer
+    switch(operation){
 
-        // code pour la multiplication 
-        resultat = first_Number*second_Number;
-        cout << "la multiplication donne: " << resultat;
+        case '+':
+            // Addition
+            cout << "Le resultat de l'addition est: " << first_Number + second_Number;
 
-    }else if(operation = '/'){
+        
+        case '-':
+            // Soustraction
+            cout << "Le resultat de la soustraction est: " << first_Number - second_Number;
 
-        // code pour la division
-        resultat = first_Number/second_Number;
-        cout << "la division donne: " << resultat;
-    
-    }else if (operation = '0')
-    {
-        // code pour l'addition
-        resultat = first_Number+second_Number;
-        cout << "l'addition donne: " << resultat;
+        case '*':
+            // Multiplication
+            cout << "Le resultat de la multiplication est: " << first_Number * second_Number;
 
-    }else if(operation = '-'){
-        // code pour la soustraction
-        resultat = first_Number-second_Number;
-        cout << "la soustraction donne: " << resultat;
-    }else{
-        cout << "Merci de rentrée une valeur! ";
+        case '/':
+            // Division
+            cout << "Le resultat de la division est: " << first_Number / second_Number;
+
+        // Sert à donner une réponse si acune case réponds à la réponse de l'utilisateur 
+        default: 
+            // Si le resultat n'est pas celui attendu (nombre ou symbole mathématique voulu)
+            cout << "Operation Invalide ";
     };
+        // Reponse de l'utilisateur afin de garder la boucle active
+       cout << "\n voulez-vous recommencer?(Y/N) ";
+            cin >> recommencer;
+
+    //boucle qui vas se repeter uniquement si l'utilisateur a choisit Y ou y comme réponse 
+    } while (recommencer == 'Y' || recommencer == 'y');
+    
 }
