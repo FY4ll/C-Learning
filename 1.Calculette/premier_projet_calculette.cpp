b789d4b7e1e61a28ea03ae6be5de9a519b2f2e7a
F4ll_ <fadyallemann@gmail.com>
2026-08-05 00:50:50 +0200

    Ajout de l'option recommander + debug

--- 1.Calculette/premier_projet_calculette.cpp
+++ 1.Calculette/premier_projet_calculette.cpp
@@ -32,28 +32,50 @@ int main() {
         case '+':
             // Addition
             cout << "Le resultat de l'addition est: " << first_Number + second_Number;
-
+             // Le break sert à arreter le switch 
+            break;
         
         case '-':
             // Soustraction
             cout << "Le resultat de la soustraction est: " << first_Number - second_Number;
+             // Le break sert à arreter le switch 
+            break;
 
         case '*':
             // Multiplication
             cout << "Le resultat de la multiplication est: " << first_Number * second_Number;
+             // Le break sert à arreter le switch 
+            break;
 
         case '/':
             // Division
             cout << "Le resultat de la division est: " << first_Number / second_Number;
+            // Le break sert à arreter le switch 
+            break;
 
         // Sert à donner une réponse si acune case réponds à la réponse de l'utilisateur 
         default: 
             // Si le resultat n'est pas celui attendu (nombre ou symbole mathématique voulu)
             cout << "Operation Invalide ";
+             // Le break sert à arreter le switch 
+            break;
     };
-        // Reponse de l'utilisateur afin de garder la boucle active
-       cout << "\n voulez-vous recommencer?(Y/N) ";
+        
+      do
+      {
+         // Reponse de l'utilisateur afin de garder la boucle active
+            cout << "\n voulez-vous recommencer?(Y/N) ";
             cin >> recommencer;
+            // verification que l'entrée soit valide et sinon on envoie un message
+            if (recommencer != 'Y' && recommencer != 'y' &&
+                recommencer != 'N' && recommencer != 'n')
+                {
+                     cout<< "Merci de mettre une entrée valide";
+                };
+      } while (recommencer != 'Y' && recommencer != 'y' &&
+         recommencer != 'N' && recommencer != 'n');
+       
+        
 
     //boucle qui vas se repeter uniquement si l'utilisateur a choisit Y ou y comme réponse 
     } while (recommencer == 'Y' || recommencer == 'y');
