bed07209dc327585c2e4ce1ed927758dbac95ca5
F4ll_ <fadyallemann@gmail.com>
2026-08-05 18:36:15 +0200

    Apprentissage des Arrays

---
+++ 2.bataile_navale/btaile_navale.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    // mise en place de l'array pour definire la position des <batteaux> 
+    bool bateaux[4][4]{
+       {1,1,0,0},
+       {0,0,0,0},
+       {0,0,1,0},
+       {0,0,1,0} 
+    };
+    int toucher; //variable qui vas stocker le nombre de <bateaux> toucher 
+    int positionX; // permet d'aller chercher dans notre array bateau l'élément voulu dans une ligne
+    int positionY; // permet d'aller chercher dans notre array bateau la ligne qui vas etre utiliser
+    do
+    {
+        // Récuperation des input utilisateur
+       cout << "Rentrer une coordonnée Y(nombre): ";
+       cin >> positionY;
+       cout << "Rentrer une coordonnée X: ";
+       cin >> positionX;
+
+        if(bateaux[positionY][positionX] = true){
+            cout << "Touché!\n";
+            toucher++;
+       }else{
+            cout << "Raté!\n";
+       };
+    } while (toucher = 4);
+    
+    
+}
