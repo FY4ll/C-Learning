214661f4a0bfe72b05d17072ac847ea25d0a2b61
F4ll_ <fadyallemann@gmail.com>
2026-08-07 07:23:07 +0200

    Utilisation de structure

---
+++ 3.data_input_struct/data_input_struct.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Personne
+{
+    string Firstname,Lastname,Username;
+    int age;
+};
+
+int main()
+{
+    bool continuer = true;
+    
+    while (continuer = true)
+    {};
+    
+}
